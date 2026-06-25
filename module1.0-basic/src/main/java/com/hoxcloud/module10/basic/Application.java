93f1d4ae0995e9f9147d1de0391d89d928f9dc03
Arun Yadav <codebyarunyadav@gmail.com>
2026-06-25 18:21:58 +0530

    basic project push to repository

---
+++ module1.0-basic/src/main/java/com/hoxcloud/module10/basic/Application.java
@@ -0,0 +1,13 @@
+package com.hoxcloud.module10.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ module1.0-basic/src/test/java/com/hoxcloud/module10/basic/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoxcloud.module10.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
