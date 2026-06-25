6d0ebc25fdbb5a45b2eb96016c2319cf3b9ca5da
Arun Yadav <codebyarunyadav@gmail.com>
2026-06-25 22:19:21 +0530

    Customise classes is annotated and DI are injected for consume from IOC beans

--- module1.0-basic/src/main/java/com/hoxcloud/module10/basic/Impl/EmailMessageService.java
+++ module1.0-basic/src/main/java/com/hoxcloud/module10/basic/Impl/EmailMessageService.java
@@ -1,7 +1,10 @@
 package com.hoxcloud.module10.basic.Impl;
 
+import org.springframework.stereotype.Component;
+
 import com.hoxcloud.module10.basic.MessageService;
 
+@Component
 public class EmailMessageService implements MessageService {
 
 	public void sendMessage(String message) {
