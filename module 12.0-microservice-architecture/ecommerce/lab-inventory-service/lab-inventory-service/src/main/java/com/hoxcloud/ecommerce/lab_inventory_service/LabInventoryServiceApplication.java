a359e33456c591db52c58681f71865b76fd936b6
Arun Yadav <codebyarunyadav@gmail.com>
2026-09-01 20:58:16 +0530

    Implement Module 12 microservice architecture

---
+++ module 12.0-microservice-architecture/ecommerce/lab-inventory-service/lab-inventory-service/src/main/java/com/hoxcloud/ecommerce/lab_inventory_service/LabInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hoxcloud.ecommerce.lab_inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabInventoryServiceApplication.class, args);
+	}
+
+}
